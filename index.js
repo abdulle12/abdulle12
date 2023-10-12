fe3157516007349d1a5460869ba744528285c349
abdulle12 <146827203+abdulle12@users.noreply.github.com>
2023-10-12 10:12:32 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 
+const names = ["John", "Alice", "Bob", "Jasmine", "Michael"];
+
+// Loop through the array
+for (let i = 0; i < names.length; i++) {
+    // Get the current name
+    const name = names[i];
+
+    // Check if the name starts with 'j' or 'J'
+    if (name.charAt(0) === 'j' || name.charAt(0) === 'J') {
+        console.log("Goodbye " + name);
+    } else {
+        console.log("Hello " + name);
+    }
+}
+
